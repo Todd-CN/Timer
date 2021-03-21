373eb301f10462c21a9b125d970dbf8a94a320c3
Todd Sutherland <todd.sutherland@cliniciannexus.com>
2021-03-21 10:41:10 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener("click", this.start);
+  }
+
+  start() {
+    console.log(this);
+  }
+}
+
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton);
+timer.start(); // results in the instance of the class created- this is what we want- we have access to all of the methods in the class
